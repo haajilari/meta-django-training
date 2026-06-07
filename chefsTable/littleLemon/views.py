c8119936c0b0654d144caa624a51beebcd157e25
Amirhossein Hajilari <56062640+haajilari@users.noreply.github.com>
2026-06-07 07:38:50 +0330

    little lemon ---- app corrections

--- chefsTable/littleLemon/views.py
+++ chefsTable/littleLemon/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+# from .models import Menu
 # Create your views here.
+
+def hello(request):
+    return HttpResponse("Hello, SAG!")
+
+# def menu_by_id(request, menu_id):
+#     menu
