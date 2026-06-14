f7b0c9eb103eeab2b16fd6e6104dc7df2fd400c7
Amirhossein Hajilari <56062640+haajilari@users.noreply.github.com>
2026-06-14 07:32:24 +0330

    make migrations :)

--- chefsTable/littleLemon/admin.py
+++ chefsTable/littleLemon/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 
+from .models import Menu, MenuCategory
+
 # Register your models here.
 
+admin.site.register(MenuCategory)
+admin.site.register(Menu)
--- chefsTable/littleLemon/models.py
+++ chefsTable/littleLemon/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+# Menu Category
+class MenuCategory(models.Model):
+    menu_category_name = models.CharField(max_length=255)
+
+class Menu(models.Model):
+    menu_item= models.CharField(max_length=255)
+    price = models.IntegerField(null=False)
+    category_id = models.ForeignKey(MenuCategory, on_delete=models.PROTECT,default=None)
+                                    
