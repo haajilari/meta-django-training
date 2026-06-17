b44a72fbb2342312b70dcb770f6451287df49891
Amirhossein Hajilari <56062640+haajilari@users.noreply.github.com>
2026-06-17 15:27:14 +0330

    testing

--- chefsTable/littleLemon/tests.py
+++ chefsTable/littleLemon/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
-
+from datetime import datetime
+from .models import Reservation
 # Create your tests here.
+
+
+
+class ReservationModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.reservation = Reservation.objects.create(
+            name="John",
+            contact="09213123",
+            count=1,
+            notes="nothing else matters"
+        )
+
+    def test_fields(self):
+        self.assertIsInstance(self.reservation.name, str)
+        self.assertIsInstance(self.reservation.contact,str)
+        self.assertIsInstance(self.reservation.count, int)
+        self.assertIsInstance(self.reservation.notes,str)
